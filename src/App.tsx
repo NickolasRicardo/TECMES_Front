a98c8e0e13f1f85ace4e2aa56e3b1dd0d09411c7
NickolasAguiar <nickolas.aguiar@credisis.com.br>
2023-07-21 07:48:12 -0300

    Finalizando estilização das telas

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ import AppProviders from "./hook";
 import Router from "./router";
 
 import Global from "./@styles/global";
+import { ConfigProvider } from "antd";
+import ptBRAnt from "antd/lib/locale/pt_BR";
+
 import { Layout } from "./layout";
 
 const App = () => (
-  <BrowserRouter>
-    <AppProviders>
-      <Global />
-      <Router />
-    </AppProviders>
-  </BrowserRouter>
+  <ConfigProvider locale={ptBRAnt}>
+    <BrowserRouter>
+      <AppProviders>
+        <Global />
+        <Router />
+      </AppProviders>
+    </BrowserRouter>
+  </ConfigProvider>
 );
 
 export default App;
